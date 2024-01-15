9084084bf8704fc8817bce564daf5103c66aa485
thaybrrt <thaybarreto@live.com>
2024-01-15 10:56:30 -0300

    att Relacionamento entre as Classes Tema e Postagem

--- src/main/java/com/generation/blogpessoal/model/Tema.java
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -1,9 +1,16 @@
 package com.generation.blogpessoal.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
@@ -20,6 +27,9 @@ public class Tema {
 	@NotNull(message = "O Atributo Descrição é obrigatório")
 	private String descricao;
 
+	@OneToMany (fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
 
 	public Long getId() {
 		return id;
@@ -38,6 +48,16 @@ public class Tema {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
 	}
 	
 
